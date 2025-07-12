13b9afdcfbd0ef1ad91d5256921bb6866927ec53
Guptaranjeet9682 <guptaranjeet9682@gmail.com>
2025-07-12 10:30:15 +0530

    Update generate-key.js

--- api/generate-key.js
+++ api/generate-key.js
@@ -1,11 +1,10 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
-module.exports = (req, res) => {
-  // Generate a token that expires in 24 hours
+export default function handler(req, res) {
   const token = jwt.sign(
     { exp: Math.floor(Date.now() / 1000) + 86400 },
     process.env.JWT_SECRET
   );
   
   res.status(200).json({ key: token });
-};
+}
